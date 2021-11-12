779c3601073e6ee016f042fd43116732d672063a
mpnogaj <mpnogaj@gmail.com>
2021-11-12 12:46:02 +0100

    Test API controller, DbContext init

---
+++ KormoranAdminSystemRevamped/Controllers/PingController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KormoranAdminSystemRevamped.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PingController : ControllerBase
+    {
+        [HttpGet]
+        public string Ping()
+        {
+            return "Pong";
+        }
+    }
+}
--- KormoranAdminSystemRevamped/Startup.cs
+++ KormoranAdminSystemRevamped/Startup.cs
@@ -1,18 +1,33 @@
+using System.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using System.IO;
-using Microsoft.AspNetCore.SpaServices.ReactDevelopmentServer;
+using Microsoft.Extensions.Configuration;
+using KormoranAdminSystemRevamped.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace KormoranAdminSystemRevamped
 {
     public class Startup
     {
         private const string PolicyName = "KormoranPolicy";
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+	        Configuration = configuration;
+        }
 
         public void ConfigureServices(IServiceCollection services)
         {
+	        services.AddDbContext<MyDbContext>(options =>
+	        {
+		        options.UseMySQL(Configuration.GetConnectionString("DefaultConnection"));
+	        });
+            services.AddControllersWithViews();
             services.AddCors(options =>
             {
                 options.AddPolicy(PolicyName, builder =>
@@ -20,7 +35,6 @@ namespace KormoranAdminSystemRevamped
                     builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
                 });
             });
-            services.AddMvc(option => option.EnableEndpointRouting = false);
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
@@ -29,9 +43,22 @@ namespace KormoranAdminSystemRevamped
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
             app.UseStaticFiles();
+            app.UseSpaStaticFiles();
             app.UseCors(PolicyName);
-            app.UseMvc();
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    "default",
+                    "{controller}/{action=Index}");
+            });
+
             app.UseSpa(spa =>
             {
                 spa.Options.SourcePath = Path.Join(env.ContentRootPath, "ClientApp");
