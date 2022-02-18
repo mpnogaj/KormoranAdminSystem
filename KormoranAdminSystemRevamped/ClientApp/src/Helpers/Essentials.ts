f4a71866b0cd7f372e8db5a8cecaeba242f9cd99
mpnogaj <mpnogaj@gmail.com>
2022-02-18 10:35:13 +0100

    Simplified team management

--- KormoranAdminSystemRevamped/ClientApp/src/Helpers/Essentials.ts
+++ KormoranAdminSystemRevamped/ClientApp/src/Helpers/Essentials.ts
@@ -2,3 +2,16 @@ export function isInIFrame() {
 	if(window == null) return false;
 	return window != window.top;
 }
+
+export function binsearch<T>(array: Array<T>, predicate: (item: T) => number){
+	var l = 0, p = array.length - 1;
+	while(l < p){
+		const sr = Math.floor((l + p) / 2);
+		const verdict = predicate(array[sr]);
+		if(verdict == 0) return sr;
+		else if(verdict < 0) l = sr + 1;
+		else p = sr - 1;
+	}
+	if(predicate(array[l]) != 0) return -1;
+	return l;
+}
--- KormoranAdminSystemRevamped/ClientApp/src/Pages/Panel/Tournaments/EditTournament.tsx
+++ KormoranAdminSystemRevamped/ClientApp/src/Pages/Panel/Tournaments/EditTournament.tsx
@@ -10,6 +10,8 @@ import ITeam from "../../../Models/ITeam";
 import axios from "axios";
 import ITournament from "../../../Models/ITournament";
 import ModalHeader from "react-bootstrap/esm/ModalHeader";
+import { idText } from "typescript";
+import { binsearch } from "../../../Helpers/Essentials";
 
 
 interface IParams {
@@ -22,9 +24,10 @@ interface ICompProps {
 
 interface ICompState {
 	isLoading: boolean,
-	selectedTeamId: number | undefined,
-	editTeamName: string | undefined,
-	editTeamModal: boolean,
+	selectedIndex: number | undefined,
+	teamTempName: string | undefined,
+	teamModal: boolean,
+	isEditModal: boolean,
 	tournamentData: ITournamentData;
 }
 
@@ -38,17 +41,16 @@ interface ITournamentData {
 
 class EditTournament extends React.Component<ICompProps, ICompState>{
 	private isEdit: boolean;
-	private storage: ElementStorage
 
 	constructor(props: ICompProps) {
 		super(props);
 		this.isEdit = this.props.params.id > 0;
-		this.storage = ElementStorage.getInstance();
 		this.state = {
 			isLoading: true,
-			editTeamModal: false,
-			selectedTeamId: undefined,
-			editTeamName: undefined,
+			teamModal: false,
+			selectedIndex: undefined,
+			teamTempName: undefined,
+			isEditModal: false,
 			tournamentData: {
 				tournament: undefined,
 				matches: [],
@@ -70,179 +72,183 @@ class EditTournament extends React.Component<ICompProps, ICompState>{
 			this.setState({
 				tournamentData: val.data,
 				isLoading: false
-			})
+			});
 		}).catch(ex => console.error(ex));
+	}
+
+	findTeamIndex = (teamId: number) => {
+		return binsearch<ITeam>(this.state.tournamentData.teams, (x) => {
+			if (x.id < teamId) return -1;
+			if (x.id == teamId) return 0;
+			return 1;
+		});
+	}
+
+	handleShow = (show: boolean, isEdit: boolean) => {
+		if (show) {
+			this.setState(prevState => ({
+				...prevState,
+				teamModal: show,
+				isEditModal: isEdit
+			}));
+		}
+		else this.setState(prevState => ({
+			...prevState,
+			teamModal: show
+		}));
+	}
+
+	updateTeams = (operation: number) => {
+
+		const newTeams = this.state.tournamentData.teams.slice();
+		const index = this.state.selectedIndex!
+
+		if (operation == -1) {
+			if (window.confirm("Czy na pewno chcesz usunąć tę druynę")) 
+				newTeams.splice(index, 1);
+			this.setState(prevState => ({
+					...prevState,
+					selectedIndex: undefined
+			}));
+		} else if (operation == 0) {
+			const newName = prompt("Nowa nazwa drużyny", newTeams[index].name);
+			if (newName == null) return;
+			newTeams[index].name = newName
+		} else {
+			const newName = prompt("Nowa nazwa drużyny");
+			if (newName == null) return;
+			newTeams.push({
+				id: -1,
+				name: newName
+			});
+		}
+
+		this.setState(prevState => ({
+			...prevState,
+			tournamentData: {
+				...prevState.tournamentData,
+				teams: newTeams
+			}
+		}));
 	}
 
 	render() {
 		return (
-			<div>
-				<div className="container mt-3">
-					<div className="logo-container">
-						<p>{this.isEdit ? "Edytuj" : "Dodaj"} turniej</p>
-					</div>
-					{
-						!this.state.isLoading
-							?
-							<div>
-								<div className="flexbox inline-d">
-									<span>Nazwa: </span>
-									<input type="text" value={this.state.tournamentData.tournament!.name}
-										onChange={(event) => {
-											{/*iks de*/ }
-											this.setState(prevState => ({
-												...prevState,
-												tournamentData: {
-													...prevState.tournamentData,
-													tournament: {
-														...prevState.tournamentData.tournament!,
-														name: event.target.value
-													}
+			<div className="container mt-3">
+				<div className="logo-container">
+					<p>{this.isEdit ? "Edytuj" : "Dodaj"} turniej</p>
+				</div>
+				{
+					!this.state.isLoading
+						?
+						<div>
+							<div className="flexbox inline-d">
+								<span>Nazwa: </span>
+								<input type="text" value={this.state.tournamentData.tournament!.name}
+									onChange={(event) => {
+										{/*iks de*/ }
+										this.setState(prevState => ({
+											...prevState,
+											tournamentData: {
+												...prevState.tournamentData,
+												tournament: {
+													...prevState.tournamentData.tournament!,
+													name: event.target.value
 												}
-											}))
-										}}></input>
-								</div>
-								<div className="mt-3 flexbox inline-d">
-									<span>Dyscyplina: </span>
-									<select value={this.state.tournamentData.tournament!.disciplineId}
-										onChange={(event) => {
-											this.setState(prevState => ({
-												...prevState,
-												tournamentData: {
-													...prevState.tournamentData,
-													tournament: {
-														...prevState.tournamentData.tournament!,
-														disciplineId: +event.target.value
-													}
+											}
+										}))
+									}}></input>
+							</div>
+							<div className="mt-3 flexbox inline-d">
+								<span>Dyscyplina: </span>
+								<select value={this.state.tournamentData.tournament!.disciplineId}
+									onChange={(event) => {
+										this.setState(prevState => ({
+											...prevState,
+											tournamentData: {
+												...prevState.tournamentData,
+												tournament: {
+													...prevState.tournamentData.tournament!,
+													disciplineId: +event.target.value
 												}
-											}));
-										}}>
-										{
-											this.state.tournamentData.disciplines.map((disc) => {
-												return <option key={disc.id} value={disc.id}>{disc.name}</option>
-											})
-										}
-									</select>
-								</div>
-								<div className="mt-3 flexbox inline-d">
-									<span>Stan: </span>
-									<select value={this.state.tournamentData.tournament!.stateId}
-										onChange={(event) => {
-											this.setState(prevState => ({
-												...prevState,
-												tournamentData: {
-													...prevState.tournamentData,
-													tournament: {
-														...prevState.tournamentData.tournament!,
-														stateId: +event.target.value
-													}
+											}
+										}));
+									}}>
+									{
+										this.state.tournamentData.disciplines.map((disc) => {
+											return <option key={disc.id} value={disc.id}>{disc.name}</option>
+										})
+									}
+								</select>
+							</div>
+							<div className="mt-3 flexbox inline-d">
+								<span>Stan: </span>
+								<select value={this.state.tournamentData.tournament!.stateId}
+									onChange={(event) => {
+										this.setState(prevState => ({
+											...prevState,
+											tournamentData: {
+												...prevState.tournamentData,
+												tournament: {
+													...prevState.tournamentData.tournament!,
+													stateId: +event.target.value
 												}
-											}));
-										}}>
-										{
-											this.state.tournamentData.states.map((state) => {
-												return <option key={state.id} value={state.id}>{state.name}</option>
-											})
-										}
-									</select>
-								</div>
-								<div className="mt-3">
-									<span>Drużyny biorące udział: </span><br />
-									<select value={this.state.selectedTeamId} size={7} style={{ width: "200px" }}
+											}
+										}));
+									}}>
+									{
+										this.state.tournamentData.states.map((state) => {
+											return <option key={state.id} value={state.id}>{state.name}</option>
+										})
+									}
+								</select>
+							</div>
+							<div className="mt-3">
+								<span>Drużyny biorące udział: </span><br />
+								<form>
+									<select value={this.state.selectedIndex} size={7} style={{ width: "200px" }}
 										onChange={(event) => {
-											const teamName = this.state.tournamentData.teams[+event.target.value - 1].name
 											this.setState((prevState) => ({
 												...prevState,
-												selectedTeamId: +event.target.value,
-												editTeamName: teamName
+												selectedIndex: +event.target.value,
 											}))
 										}}>
 										{
-											this.state.tournamentData.teams.map((team) => {
-												return <option key={team.id} value={team.id}>{team.name}</option>
+											this.state.tournamentData.teams.map((team, index) => {
+												return <option key={team.id} value={index}>{team.name}</option>
 											})
 										}
 									</select>
-									<div className="mt-2 flexbox inline-d">
-										<Button className="me-2" variant="success">Dodaj nową</Button>
-										<Button className="me-2" variant="success">Usuń zaznaczoną</Button>
-										<Button disabled={this.state.selectedTeamId == undefined}
-											className="me-2" variant="success"
-											onClick={(event) => this.setState(prevState => ({
+									<div>
+										<input className="btn btn-secondary" type="reset" value="Wyczyść zaznaczenie"
+											style={{width: "200px"}}
+											onClick={() => this.setState(prevState => ({
 												...prevState,
-												editTeamModal: true
-											}))}>Edytuj zaznaczoną</Button>
+												selectedIndex: undefined
+											}))}/>
 									</div>
+								</form>
+								<div className="mt-3 flexbox inline-d">
+									<Button className="me-2" variant="success"
+										onClick={() => this.updateTeams(1)}>Dodaj nową</Button>
+									<Button disabled={this.state.selectedIndex == undefined}
+										className="me-2" variant="success"
+										onClick={() => this.updateTeams(-1)}>Usuń zaznaczoną</Button>
+									<Button disabled={this.state.selectedIndex == undefined}
+										className="me-2" variant="success"
+										onClick={() => this.updateTeams(0)}>Edytuj zaznaczoną</Button>
 								</div>
-								<div className="mt-3">
-									<Table responsive={true}>
-
-									</Table>
-								</div>
-								<h1>{this.props.params.id}</h1>
 							</div>
-							:
-							<h1>Ładowanie</h1>
-					}
-				</div>
-				<Modal show={this.state.editTeamModal} onShow={() => {
-					if (this.state.editTeamName == undefined) {
-						const index = this.state.selectedTeamId! - 1;
-						const name = this.state.tournamentData.teams[index].name;
-						this.setState(prevState => ({
-							...prevState,
-							editTeamName: name
-						}));
-					}
-				}} onHide={() => {
-					this.setState(prevState => ({
-						...prevState,
-						editTeamModal: false,
-						editTeamName: undefined
-					}));
-				}}>
-					<ModalHeader closeButton>
-						<Modal.Title>Edytuj drużynę</Modal.Title>
-					</ModalHeader>
-					<ModalBody>
-						<div className="flexbox inline-d">
-							<span>Nazwa: </span>
-							<input type="text" value={
-								this.state.editTeamName == undefined ? "" :
-									this.state.editTeamName
-							}
-								onChange={(event) => {
-									if (this.state.selectedTeamId == undefined) return;
-									this.setState(prevState => ({
-										...prevState,
-										editTeamName: event.target.value
-									}))
-								}}></input>
+							<div className="mt-3">
+								<Table responsive={true}>
+
+								</Table>
+							</div>
+							<h1>{this.props.params.id}</h1>
 						</div>
-					</ModalBody>
-					<ModalFooter>
-						<Button onClick={async () => {
-							const newTeams = this.state.tournamentData.teams.slice();
-							newTeams[this.state.selectedTeamId! - 1].name = this.state.editTeamName!;
-							this.setState(prevState => ({
-								...prevState,
-								tournamentData: {
-									...prevState.tournamentData,
-									teams: newTeams
-								},
-								editTeamModal: false,
-								editTeamName: undefined
-							}));
-						}}>Ok</Button>
-						<Button onClick={() => {
-							this.setState(prevState => ({
-								...prevState,
-								editTeamModal: false,
-								editTeamName: undefined
-							}));
-						}}>Anuluj</Button>
-					</ModalFooter>
-				</Modal>
+						:
+						<h1>Ładowanie</h1>
+				}
 			</div>
 		)
 	}
