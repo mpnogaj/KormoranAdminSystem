e06b14d9adbf85bdd797515ba6038202992df823
mpnogaj <mpnogaj@gmail.com>
2022-04-03 17:54:24 +0200

    Added toast messages

---
+++ KormoranMobile/KormoranMobile.Android/Impl/ToastMessageService.cs
@@ -0,0 +1,17 @@
+﻿using Android.Widget;
+using KormoranMobile.Droid.Impl;
+using KormoranMobile.Services;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ToastMessageService))]
+namespace KormoranMobile.Droid.Impl
+{
+    public class ToastMessageService : IToastMessageService
+    {
+        public void ShowToast(string message)
+        {
+            Toast.MakeText(Platform.AppContext, message, ToastLength.Long);
+        }
+    }
+}
--- KormoranMobile/KormoranMobile/ViewModels/MainPageViewModel.cs
+++ KormoranMobile/KormoranMobile/ViewModels/MainPageViewModel.cs
@@ -8,28 +8,42 @@ namespace KormoranMobile.ViewModels
     internal class MainPageViewModel : ViewModelBase
     {
         private IRequesterService _requester;
+        private IToastMessageService _toaster;
         public AsyncRelayCommand<string> UpdateScore { get; private set; }
 
         private string _matchId;
         public string MatchId { get => _matchId; set => SetProperty(ref _matchId, value); }
 
         private string _points;
-        public string Points { get => _points; set => SetProperty(ref _points, value);
-        }
+        public string Points { get => _points; set => SetProperty(ref _points, value); }
 
         public MainPageViewModel()
         {
             _requester = DependencyService.Get<IRequesterService>(DependencyFetchTarget.GlobalInstance);
+            _toaster = DependencyService.Get<IToastMessageService>(DependencyFetchTarget.GlobalInstance);
             UpdateScore = new AsyncRelayCommand<string>(async (string team) =>
             {
-                var res = await _requester.SendPost<BasicResponse>("/api/matches/IncrementScore", new RequestModel
+                try
                 {
-                    MatchId = Convert.ToInt32(_matchId),
-                    Value = Convert.ToInt32(_points),
-                    Team = Convert.ToInt32(team)
-                });
-
-
+                    var res = await _requester.SendPost<BasicResponse>("/api/matches/IncrementScore", new RequestModel
+                    {
+                        MatchId = Convert.ToInt32(_matchId),
+                        Value = Convert.ToInt32(_points),
+                        Team = Convert.ToInt32(team)
+                    });
+                    if (res.Error)
+                    {
+                        _toaster.ShowToast($"Wystapił błąd. Treść: {res.Message}");
+                    }
+                    else
+                    {
+                        _toaster.ShowToast("Operacja zakończona sukcesem");
+                    }
+                }
+                catch (Exception ex)
+                {
+                    _toaster.ShowToast($"Wystapił błąd. Treść: {ex.Message}");
+                }
             }, () => true);
         }
     }
