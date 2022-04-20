d8f55f74e0844e2574f7afb4fce071e8c2d59661
mpnogaj <mpnogaj@gmail.com>
2022-04-20 17:06:20 +0200

    Added maui default project

---
+++ KormoranMobile.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace KormoranMobile.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
--- KormoranMobile/KormoranMobile.UWP/App.xaml.cs
+++ KormoranMobile/KormoranMobile.UWP/App.xaml.cs
@@ -10,7 +10,7 @@ namespace KormoranMobile.UWP
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    internal sealed partial class App : Application
+    sealed partial class App : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
