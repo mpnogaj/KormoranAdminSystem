1cef6ba55376ce075a1d4f160385b5d61a5beb25
Marcin Nogaj <mpnogaj@gmail.com>
2022-04-20 14:20:53 +0200

    Added svg frame control [mobile app]

---
+++ KormoranMobile/KormoranMobile/ViewModels/LoginPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace KormoranMobile.ViewModels
+{
+	public class LoginPageViewModel : ViewModelBase
+	{
+		private string _username;
+		public string Username { get => _username; set => SetProperty(ref _username, value); }
+		
+		private string _password;
+		public string Password { get => _password; set => SetProperty(ref _password, value); }
+	}
+}
